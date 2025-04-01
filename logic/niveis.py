4ccb37e05023b7e895dbcbfa9a7f7011556b2467
stehkroll <stekakr@gmail.com>
2025-04-01 19:42:09 -0300

    Update niveis.py

--- logic/niveis.py
+++ logic/niveis.py
@@ -1,11 +1,14 @@
-# logic/niveis.py
-
-def calcular_nivel(xp_total):
+def calcular_nivel(pontos):
+    xp_total = pontos
     nivel = 1
     xp_necessario = 100
+
     while xp_total >= xp_necessario and nivel < 99:
         xp_total -= xp_necessario
         nivel += 1
-        xp_necessario += 10
-    progresso = xp_total / xp_necessario
-    return nivel, progresso, xp_necessario
+        xp_necessario = int(xp_necessario * 1.1)
+
+    xp_atual = xp_total
+    progresso = int((xp_atual / xp_necessario) * 100)
+
+    return nivel, xp_atual, xp_necessario, progresso
