f9f2041d5bec7d4d674e7385a175390d9fdf6d2b
stehkroll <stekakr@gmail.com>
2025-04-01 20:17:02 -0300

    Update painel_recompensas.py

--- components/painel_recompensas.py
+++ components/painel_recompensas.py
@@ -5,6 +5,7 @@ def mostrar_painel_recompensas(pontos_disponiveis):
     st.subheader("🎁 Recompensas")
 
     recompensas = pd.read_csv("data/recompensas.csv")
+    print("Colunas carregadas:", recompensas.columns.tolist())
     recompensas.columns = recompensas.columns.str.strip()
 
     cores_borda = {
