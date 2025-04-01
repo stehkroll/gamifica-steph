27f2ca56938c50aaeaea5bd1e212f735384bd0c5
stehkroll <stekakr@gmail.com>
2025-04-01 18:14:13 -0300

    Create personagem.py

---
+++ components/personagem.py
@@ -0,0 +1,11 @@
+assets/personagem/
+├── corpo_base.png
+├── roupa_base.png
+├── olhos/
+│   ├── castanho.png
+│   ├── azul.png
+│   └── ...
+└── cabelo/
+    ├── curto1_preto.png
+    ├── longo_cacheado_roxo.png
+    └── ...
