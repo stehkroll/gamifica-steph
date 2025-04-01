ccd83ebf7c58bcc15096c3b0578af9f5685e39e6
stehkroll <stekakr@gmail.com>
2025-04-01 18:21:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,34 @@ st.subheader(f"📊 Nível {nivel}")
 st.progress(progresso)
 st.caption(f"Você está a {xp_proximo_nivel - xp_atual} XP de alcançar o nível {nivel + 1}!")
 
+#personagem
+st.markdown("---")
+st.subheader("🧍 Personalização do Personagem")
+
+# Escolhas do usuário
+olho_escolhido = st.selectbox("Escolha a cor dos olhos:", [
+    "castanho", "azul", "verde", "roxo", "vermelho", "rosa"
+])
+
+estilo_cabelo = st.selectbox("Escolha o estilo de cabelo:", [
+    "sem_cabelo",
+    "curto1", "curto2",
+    "medio_liso", "medio_cacheado",
+    "longo_liso", "longo_cacheado"
+])
+
+cor_cabelo = st.selectbox("Escolha a cor do cabelo:", [
+    "preto", "castanho", "vermelho", "rosa", "roxo",
+    "azul", "verde", "loiro", "branco"
+])
+
+# Combina estilo com cor
+cabelo_escolhido = f"{estilo_cabelo}_{cor_cabelo}"
+
+# Mostra personagem com as escolhas feitas
+montar_personagem(olho=olho_escolhido, cabelo=cabelo_escolhido)
+
+
 # Salva progresso diário
 progresso_df = pd.DataFrame([[hoje, pontos, nivel]], columns=["Data", "Pontos", "Nivel"])
 progresso_df.to_csv("data/progresso.csv", index=False)
