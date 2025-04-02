0b7e86a5eae7012612707b3c2ef7b40924bff8ed
stehkroll <stekakr@gmail.com>
2025-04-02 20:25:40 -0300

    update
    teste

--- components/painel_recompensas.py
+++ components/painel_recompensas.py
@@ -19,5 +19,7 @@ def mostrar_painel_recompensas(_):
                     st.session_state.pontos_totais -= row["Pontos"]
                     salvar_pontos()
                     st.success(f"🎉 Recompensa desbloqueada: {row['Nome']}")
+                    st.experimental_rerun()
+
             else:
                 st.info(f"🔒 Faltam {row['Pontos'] - st.session_state.pontos_totais} pontos")
