b2888bfe5e7c2c88c2ed033266426a173c821c24
stehkroll <stekakr@gmail.com>
2025-04-03 13:46:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -126,5 +126,5 @@ elif pagina == "Dia Atual":
     progresso_df.to_csv("data/progresso.csv", index=False)
 
     st.markdown("---")
-    mostrar_painel_recompensas(st.session_state.pontos_totais)
+    mostrar_painel_recompensas()
 
